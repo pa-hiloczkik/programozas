21d0ea8aaf316784ac7381f65e8838dfd9446860
pa-hiloczkik <112852267+pa-hiloczkik@users.noreply.github.com>
2022-09-26 08:15:54 +0200

    Add files via upload

---
+++ valtozo.py
@@ -0,0 +1,18 @@
+from re import T
+
+
+x = 5
+y = "John"
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+x = 6.2
+print (x)
+print (type(x))
+x = 4 # x is a type int
+print (x)
+print(type(x))
+x = "Sally" # x is now of type str
+print(x)
+print(type(x))
