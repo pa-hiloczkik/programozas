ed99831aa88724ae504623c2965245a88519a9dd
pa-hiloczkik <112852267+pa-hiloczkik@users.noreply.github.com>
2022-10-10 09:39:45 +0200

    Create 11_12_ora.py

---
+++ 11_12_ora.py
@@ -0,0 +1,24 @@
+"""
+gondolt_szám = 4
+tipp = input('Gondoltam egy számra. Tippeld meg!')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print('Ügyes!')
+if tipp > gondolt_szám: 
+    print('A tipp nagyobb mint a gondolt szám.')
+if tipp < gondolt_szám:
+    print('A tipp kisebb mint a gondolt szám.')
+
+
+    print('Szia!')
+"""
+
+
+gondolt_szám = 4
+tipp = input('Gondoltam egy számra. Tippeld meg!')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print('Ügyes!')
+else : 
+    print('Nem találtad el!')
+print('PÁPÁ!!')
