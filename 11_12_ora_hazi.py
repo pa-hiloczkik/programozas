9d5d75ad76af42374f637e17aed21f951e67b3f1
pa-hiloczkik <112852267+pa-hiloczkik@users.noreply.github.com>
2022-10-11 13:59:42 +0200

    Update 11_12_ora_hazi.py

--- 11_12_ora_hazi.py
+++ 11_12_ora_hazi.py
@@ -1,9 +1,7 @@
 egyikszam=int(input("Kérem az első számot 1-100-ig: "))
 masodikszam=int(input("Kérem a második számot 1-100-ig: "))
 if egyikszam>masodikszam:
-    print("Az első szám nagyobb")
-    print("Ennyivel:",egyikszam-masodikszam)
+    print("Az első szám nagyobb,a különbségük",egyikszam-masodikszam)
 elif egyikszam<masodikszam:
-    print("A második szám nagyobb")
-    print("Ennyivel:",masodikszam-egyikszam)
+    print("A második szám nagyobb, különbségük",masodikszam-egyikszam)
 else: print("Megegyezik a két szám.")
