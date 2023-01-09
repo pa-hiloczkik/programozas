cef4d42ad2b1e32112c8b8d43451b7b1a70c0e93
pa-hiloczkik <112852267+pa-hiloczkik@users.noreply.github.com>
2023-01-09 09:12:06 +0100

    Create eletszakaszok.py

---
+++ eletszakaszok.py
@@ -0,0 +1,38 @@
+'''
+nev = (input('Kérek egy nevet:'))
+eletkor = int(input('Kérek egy életkort:'))
+if eletkor < 2:
+    print('A kora alapján',nev,'Csecsemő')
+elif eletkor < 6:
+        print('A kora alapján',nev,'Kisgyerek')
+elif eletkor < 12:
+        print('A kora alapján',nev,'gyerek')
+elif eletkor < 16:
+        print('A kora alapján',nev,'Serdülő')
+elif eletkor < 25:
+        print('A kora alapján',nev,'Ifjú')
+elif eletkor < 65:
+        print('A kora alapján',nev,'Felnőtt')
+elif eletkor > 65:
+        print('A kora alapján',nev,'Nyugdíjas')
+'''
+
+#------------------------------------------------------------------------------------------------------------
+
+
+nev = (input('Kérek egy nevet: '))
+kor = int(input('Kérek egy életkort: '))
+if kor < 1:
+    print('A kora alapján',nev,'csecsemő')
+elif kor < 6:
+        print('A kora alapján',nev,'kisgyerek')
+elif kor < 12:
+        print('A kora alapján',nev,'gyerek')
+elif kor < 16:
+        print('A kora alapján',nev,'serdülő')
+elif kor < 25:
+        print('A kora alapján',nev,'ifjú')
+elif kor < 65:
+        print('A kora alapján',nev,'felnőtt')
+else:
+        print('A kora alapján',nev,'nyugdíjas')
