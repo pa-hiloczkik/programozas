17970b7a737776549fbde7deeb67ccffbad2f991
pa-hiloczkik <112852267+pa-hiloczkik@users.noreply.github.com>
2023-03-23 08:07:06 +0100

    Create 03-23.py

---
+++ 03-23.py
@@ -0,0 +1,19 @@
+#ord()    A megadott karakter Unicode-ját képviselő egész szám konvertálása
+x = ord("A") #97
+
+#chr()    Egy karaktert ad vissza a megadott Unicode kódból.
+x = chr(97)
+
+#min()    Az iterálható legkisebb elemet adja vissza
+x = min(5, 10)
+
+#max()    Az iterálható legnagyobb elemet adja vissza
+x = max(5, 10)
+
+#index()   Az index() metódus a pozíciót adja vissza a megadott érték első előfordulásakor.
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.index("cherry")
+
+#list()     Listát ad vissza.
+ezalista = ["apple", "banana", "cherry"]
+print(ezalista)
